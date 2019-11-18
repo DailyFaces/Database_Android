57b48b24387336db3799d0be891d6a9a4cf9a300
josephine-pn3ed <54094282+josephine-pn3ed@users.noreply.github.com>
2019-11-18 18:43:21 +0800

    Delete 1573818113000_create_table_users.js

--- migrations/1573818113000_create_table_users.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    "up": "CREATE TABLE accounts (id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, username VARCHAR(32) UNIQUE NOT NULL, email VARCHAR(32) NOT NULL, password VARCHAR(32) NOT NULL, type VARCHAR(32) NOT NULL, created_at VARCHAR(32) NOT NULL, updated_at VARCHAR(32), deleted_at VARCHAR(32))",
-    "down": "DROP TABLE accounts"
-}
