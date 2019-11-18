f4b9ea0fffd1edcadf5eac213e9ab6a7a93208cf
josephine-pn3ed <54094282+josephine-pn3ed@users.noreply.github.com>
2019-11-18 18:43:32 +0800

    Delete 1573818757820_create_table_users.js

--- migrations/1573818757820_create_table_users.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    "up": "UPDATE accounts SET username = 'John Snow' WHERE username = 'John'",
-    "down": "UPDATE accounts SET username = 'John' WHERE username = 'John Snow'"
-}
