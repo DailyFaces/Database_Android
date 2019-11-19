77affd97d290ca712ebe49a275fcb6f5663b7c8d
josephine-pn3ed <54094282+josephine-pn3ed@users.noreply.github.com>
2019-11-19 14:07:47 +0800

    Update migration.js

--- migration.js
+++ migration.js
@@ -1,9 +1,5 @@
 var mysql = require('mysql');
 var migration = require('mysql-migrations');
-var app = require('express')();
-var express = require('express');
-var http = require('http').createServer(app);
-var port = process.env.PORT || 3000;
 
 var connection = mysql.createPool({
     connectionLimit: 10,
@@ -14,7 +10,3 @@ var connection = mysql.createPool({
 });
 
 migration.init(connection, __dirname + '/migrations');
-
-http.listen(port, function () {
-    console.log('listening on *: ' + port);
-});
