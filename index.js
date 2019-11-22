c410ea11953f0640d81ad9e713ee6186d7e9f2c6
Christian Gigante <christian.gigante@student.passerellesnumeriques.org>
2019-11-22 09:35:22 +0800

    cleanUP

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
-const mysql = require("mysql");
+const PORT = process.env.PORT || 3000;
 const app = require("express")();
-const express = require("express");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const config = require('./config');
 const http = require("http").createServer(app);
-const port = process.env.PORT || 3000;
-const SECRET_KEY = "secretkey23456";
 
-//importing Account CRUD
+//imPORTing Account CRUD
 const account_create = require('./account_CRUD/create')
 const account_auth = require('./account_CRUD/auth')
 const account_auth_user = require('./account_CRUD/auth_user')
 const account_update = require('./account_CRUD/update')
 const account_delete = require('./account_CRUD/delete')
 
+console.log("connecting..,");
+
 app.get('/auth/user', function (req, res, next) {
   account_auth_user.auth_user(req, res);
 })
 
-app.post("/accounts/create", function(req, res) {
-  account_create.create_account(req,res);
+app.post("/accounts/create", function (req, res) {
+  account_create.create_account(req, res);
 });
 
-app.get("/auth", function(req, res) {
+app.get("/auth", function (req, res) {
   account_auth.auth(req, res);
 });
 
-app.put("/accounts/update", function(req, res) {
+app.put("/accounts/update", function (req, res) {
   account_update.update(req, res);
 });
 
-app.delete("/accounts/delete", function(req, res) {
+app.delete("/accounts/delete", function (req, res) {
   account_delete.account_delete(req, res);
 });
 
-http.listen(port, function() {
-  console.log("listening on *: " + port);
+http.listen(PORT, function () {
+  console.log("listening on *: " + PORT);
 });
--- system/db_connection.js
+++ system/db_connection.js
@@ -8,4 +8,5 @@ const connection = mysql.createPool({
   database: "daily_faces"
 });
 
-module.exports =  connection
+
+module.exports = connection
