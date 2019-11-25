42d38fb786d1f4fdeebf19ece71718df2e103737
Yol-B <yoltorres24@gmail.com>
2019-11-25 14:10:02 +0800

    refactored

--- index.js
+++ index.js
@@ -1,40 +1,33 @@
-const mysql = require("mysql");
 const app = require("express")();
 const express = require("express");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const config = require('./config');
-const http = require("http").createServer(app);
+const app = express(); //use express instead of http module
 const port = process.env.PORT || 3000;
-const SECRET_KEY = "secretkey23456";
+// removed unused imports
+// create connection as global
+global.connection = require('../system/db_connection')
+    //importing Account CRUD
+const account_crud = require('./account_CRUD/account_crud'); //main crud controller
 
-//importing Account CRUD
-const account_create = require('./account_CRUD/create')
-const account_auth = require('./account_CRUD/auth')
-const account_auth_user = require('./account_CRUD/auth_user')
-const account_update = require('./account_CRUD/update')
-const account_delete = require('./account_CRUD/delete')
-
-app.get('/auth/user', function (req, res, next) {
-  account_auth_user.auth_user(req, res);
+app.post('/auth/user', function(req, res, next) {
+    account_crud.auth_user(req, res);
 })
 
 app.post("/accounts/create", function(req, res) {
-  account_create.create_account(req,res);
+    account_crud.register(req, res);
 });
 
-app.get("/auth", function(req, res) {
-  account_auth.auth(req, res);
+app.post("/auth", function(req, res) {
+    account_crud.auth(req, res);
 });
 
-app.put("/accounts/update", function(req, res) {
-  account_update.update(req, res);
+app.post("/accounts/update", function(req, res) {
+    account_crud.update(req, res);
 });
 
-app.delete("/accounts/delete", function(req, res) {
-  account_delete.account_delete(req, res);
+app.post("/accounts/delete", function(req, res) {
+    account_crud.delete_account(req, res);
 });
 
-http.listen(port, function() {
-  console.log("listening on *: " + port);
+app.listen(port, function() {
+    console.log("listening on *: " + port);
 });
