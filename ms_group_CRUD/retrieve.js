203edb5d24a28cfe79373d70a4161c8b635aa8f2
Christian Gigante <christian.gigante@student.passerellesnumeriques.org>
2019-12-13 10:28:22 +0800

    Happy ENDING

--- ms_group_CRUD/retrieve.js
+++ ms_group_CRUD/retrieve.js
@@ -17,7 +17,7 @@ let all_retrieve_ms_group = (req, res) => {
         }
         if (results.length != 0) {
             response.setRespose({ message: "All_ms_groups_ Successfully Retrieved!", data: results }, null, new Date().toISOString());
-            return res.status(200).send(response);
+            return res.send(response);
         }
     });
 }
@@ -41,48 +41,41 @@ let retrieve_ms_groups = (req, res) => {
                 if (error) {
                     data.ms_groups = error
                     // response.setRespose(null, { message: "An error occured!", err: error }, new Date().toISOString());
-                    return res.status(401).send({ message: "An error occured!", err: error });
+                    return res.send({ message: "An error occured!", err: error });
                 }
 
                 //all ms_groups
                 data.ms_groups = results
-                if (data.ms_groups.length != 0) {
-                    // response.setRespose({ message: "Active ms_groups Successfully Retrieved! Here", data }, null, new Date().toISOString());
-                    //member in group
-                    //loop all ms_group
-                    // let ms_groups_id;
-                    // let stmt_ms_group_members = `SELECT * FROM ms_members WHERE id = '${ms_groups_id}'`
 
-                    data.ms_groups.forEach((element, index) => {
-                        console.log("ele");
-                        console.log(element.id);
-                        console.log("end");
+                if (results.length != 0) {
+                    //get users in specific group
+                    let stmt_ms_group_members = `SELECT * FROM ms_members `
+                    connection.query(stmt_ms_group_members, (error, members) => {
+                        // data.ms_members = members;
+                        //check ms_group_id of ms_members
+                        tempMembers = []
+                        members.forEach(ms_member => {
+                            data.ms_groups.forEach(ms_group => {
+                                ms_group["ms_member"] = []
+                                if (ms_member.ms_group_id == ms_group.id) {
+                                    ms_group["ms_member"].push(ms_member)
+                                    return;
+                                }
+                            });
+                        });
+                        // data.ms_groups = results
+                        // return res.send({ data: members });
+                        return res.send({ message: "Active ms_groups Successfully Retrieved! Here", data: data });
 
-                        let stmt_ms_group_members = `SELECT * FROM ms_members WHERE ms_group_id = '${element.id}'`
-                        var tempMembers = [];
-                        connection.query(stmt_ms_group_members, (error1, members) => {
-                            if (error1) {
-                                console.log(error1);
+                    })
 
-                            }
-                            else {
-                                console.log("meme");
-                                console.log(members);
-                                tempMembers = members
-                            }
-                            data.ms_groups[index]['ms_members'] = tempMembers;
 
-                            console.log(data);
-
-                            return res.send({ message: "Active ms_groups Successfully Retrieved! Here", data: data });
-                        })
-                    });
-
+                    // return res.send({ message: "Active ms_groups Successfully Retrieved! Here", data: data });
                 }
 
                 else {
                     // response.setRespose(null, { message: "No Group Exist!", data: data }, new Date().toISOString());
-                    return res.status(200).send({ error: false, message: "No Group Exist!", data: data });
+                    return res.send({ error: false, message: "No Group Exist!", data: data });
                 }
             });
             return;
@@ -111,18 +104,18 @@ let retrieve_ms_group = (req, res) => {
                 if (error) {
                     data.ms_groups = error
                     // response.setRespose(null, { message: "An error occured!", err: error }, new Date().toISOString());
-                    return res.status(401).send({ message: "An error occured!", err: error });
+                    return res.send({ message: "An error occured!", err: error });
                 }
                 //all ms_groups
                 data.ms_groups = results
                 if (data.ms_groups.length != 0) {
-                    return res.status(200).send(
+                    return res.send(
                         { message: "Active ms_groups Successfully Retrieved! Here", data: data }
                     );
                 }
                 else {
                     // response.setRespose(null, { message: "No Group Exist!", data: data }, new Date().toISOString());
-                    return res.status(200).send({ error: false, message: "No Group Exist!", data: data });
+                    return res.send({ error: false, message: "No Group Exist!", data: data });
                 }
             });
             return;
